df10a5e5047fc983852b40408a079deebd78c079
Mark Beech <mbeech@mark-beech.co.uk>
2015-09-03 23:39:51 +0100

    Redo strcuture and CSS

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,80 +1,68 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
+    <head>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
 
-    <title>Awesome PHP - A curated list of amazingly awesome PHP libraries, resources and shiny things.</title>
+        <title>Awesome PHP - A curated list of amazingly awesome PHP libraries, resources and shiny things.</title>
 
-    <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
 
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom CSS -->
-    <link href="css/simple-sidebar.css" rel="stylesheet">
+        <!-- Custom CSS -->
+        <link href="css/simple-sidebar.css" rel="stylesheet">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
-</head>
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
 
-<body>
+    </head>
 
-    <div class="header">
-        Awesome PHP
-    </div>
-    <div id="wrapper">
-
-        <!-- Sidebar -->
-        <div id="sidebar-wrapper">
-            <ul class="sidebar-nav">
-                @foreach($menu as $item)
-                    <li><a href="{{ preg_replace("/[^A-Za-z0-9]/", '', $item) }}">{{ $item }}</a></li>
-                @endforeach
-            </ul>
-        </div>
-        <!-- /#sidebar-wrapper -->
-
-        <!-- Page Content -->
-        <div id="page-content-wrapper">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-lg-12 main-content">
-                        {!! markdown($section) !!}
-                        {{-- <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a> --}}
-                    </div>
+    <body>
+        <div id="wrapper">
+            <div id="header">
+                <div id="header-content-right">
+                    <a href="#">Contribute <i class="fa fa-github"></i></a>
+                </div>
+                <div id="header-content">Awesome PHP</div>
+            </div>
+            <div id="content">
+                <div id="main">
+                    {!! markdown($section) !!}
+                </div>
+                <div id="sidebar">
+                    <ul class="sidebar-nav">
+                        @foreach($menu as $item)
+                            <li><a href="{{ preg_replace("/[^A-Za-z0-9]/", '-', $item) }}">{{ $item }}</a></li>
+                        @endforeach
+                    </ul>
                 </div>
             </div>
+            <div class="push"></div>
         </div>
-        <!-- /#page-content-wrapper -->
+        <div id="footer">
+            <div id="footer-content">
+                built with <i class="fa fa-heart"></i> by <a href="#">JayBizzle</a>
+            </div>
+        </div>
 
-    </div>
-    <!-- /#wrapper -->
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
 
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-    <!-- Menu Toggle Script -->
-    <script>
-    $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
-    </script>
-
-</body>
-
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+    </body>
 </html>
