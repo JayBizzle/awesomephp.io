a1aafe28a466572900a7958acfbb08f0116abed5
Mark Beech <mbeech@mark-beech.co.uk>
2015-09-03 23:40:06 +0100

    Ignore new lines when reading files

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,7 +9,7 @@ class AppServiceProvider extends ServiceProvider
 	public function boot()
     {
     	if (! \App::runningInConsole()) {
-	    	$menu = file(base_path().'/resources/content/menu.md');
+	    	$menu = file(base_path().'/resources/content/menu.md', FILE_IGNORE_NEW_LINES);
 			sort($menu);
 	        view()->share('menu', $menu);
 	    }
