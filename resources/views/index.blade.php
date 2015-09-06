8b23cdefdf24536803825d6ff779e72e89c83b0f
Mark Beech <mbeech@mark-beech.co.uk>
2015-09-06 22:56:31 +0100

    Change active link on click

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -79,6 +79,13 @@
                 ga('set', 'location', window.location.href);
                 ga('send', 'pageview');
             });
+
+            $(document).ready(function() {
+                $('.sidebar-nav a').on('click', function() {
+                    $('.sidebar-nav a').removeClass('active');
+                    $(this).addClass('active');
+                });
+            });
         </script>
 
         <script>
